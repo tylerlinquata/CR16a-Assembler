5d09e446ff65a627befe4993d062f8c75200dc6e
Tyler Linquata <tylerlinquata@gmail.com>
2019-09-26 11:49:43 -0600

    add flex tokenizer

---
+++ CR16a-Assembler/CR16a-Assembler/instruction.cpp
@@ -0,0 +1,9 @@
+//
+//  instruction.cpp
+//  CR16a-Assembler
+//
+//  Created by Tyler Linquata on 9/26/19.
+//  Copyright © 2019 Tyler Linquata. All rights reserved.
+//
+
+#include "instruction.hpp"
---
+++ CR16a-Assembler/CR16a-Assembler/instruction.hpp
@@ -0,0 +1,14 @@
+//
+//  instruction.hpp
+//  CR16a-Assembler
+//
+//  Created by Tyler Linquata on 9/26/19.
+//  Copyright © 2019 Tyler Linquata. All rights reserved.
+//
+
+#ifndef instruction_hpp
+#define instruction_hpp
+
+#include <stdio.h>
+
+#endif /* instruction_hpp */
