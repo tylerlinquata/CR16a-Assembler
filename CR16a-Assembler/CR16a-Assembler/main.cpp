ae69ce49f530dda9e16149850c3ec13bbc1112f4
Tyler Linquata <tylerlinquata@gmail.com>
2019-09-26 09:35:25 -0600

    Create main file

---
+++ CR16a-Assembler/CR16a-Assembler/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  CR16a-Assembler
+//
+//  Created by Tyler Linquata on 9/26/19.
+//  Copyright © 2019 Tyler Linquata. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    // TODO recieve instructions from commandline (for now)
+    // TODO recieve instructions from .asm file
+//     TODO tokenize
+//     TODO convert to binary
+    
+    
+    
+    
+    
+    
+    return 0;
+}
